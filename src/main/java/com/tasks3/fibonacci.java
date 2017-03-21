ffbf29b788fb01e202316a617315b722c9506d2a
opol <ulyana_opolska@epam.com>
2017-03-21 10:11:31 +0200

    week 3, task 3 - done, but doesn't pass validation; week 4 task 4 - success

--- src/main/java/com/tasks3/fibonacci.java
+++ src/main/java/com/tasks3/fibonacci.java
@@ -4,5 +4,20 @@ package com.tasks3;
  * Created by Ulyana on 20-Mar-17.
  */
 public class fibonacci {
-    
+    //Повертає число Фібоначчі за номером, нумерація почнеться з одиниці
+    //якщо число не можливо вирахувати поверніть -1
+
+    public fibonacci() {
+
+    }
+
+    ;
+
+    public long getNumber(int position) {
+        if (position == -1 || position == 0 || position == 1 || position == 2) {
+            return -1;
+        }
+        return getNumber(position - 1) + getNumber(position - 2);
+
+    }
 }
---
+++ src/main/java/week4/Comparator.java
@@ -0,0 +1,16 @@
+package week4;
+
+/**
+ * Created by Ulyana on 20-Mar-17.
+ */
+public class Comparator {
+    public int compare(int a, int b){
+        if (a>b) {
+            return 1;
+        } else if (a == b){
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
