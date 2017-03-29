fe13392ebaee51da0be572b49b683c3873424881
KYIV\Ulyana_Opolska <Ulyana_Opolska@epam.com>
2017-03-29 17:43:20 +0300

    week 6 task 2, need to add one more check for 2 numbers in a row

--- src/main/java/com/tasks6/rle_decoder/Application2.java
+++ src/main/java/com/tasks6/rle_decoder/Application2.java
@@ -4,42 +4,38 @@ public class Application2 {
     public static void main(String[] args) {
         if (args == null || args.length != 1 || args[0] == null || args[0].length() <= 0) {
             return;
-        }
-        else if (args[0].isEmpty()) {
+        } else if (args[0].isEmpty()) {
             System.out.println();
-        }
-        else {
-            char [] source = args[0].toCharArray();
+        } else {
+            char[] source = args[0].toCharArray();
 
             StringBuilder dest = new StringBuilder();
 
-            for (int i=0; i<source.length; i++){
-                String el = String.valueOf(source[i]);
-                dest.append(source[i]);
-                i++;
-                int number = Integer.parseInt(el);
-                while (i+1<source.length && number >=2 && number<=9) {
-                    i++;
+            for (int i = 0; i < source.length; i++) {
+                if (i>=1 && source[i] == source[i - 1]) {
+                    System.out.println();
+                    return;
                 }
-                dest.append(source[i-1]);
-            }
-                /*try
-                {
-                    int number = Integer.parseInt(el);
-                    while (i+1<source.length && number >=2 && number<=9) {
-                        i++;
-                    }
-                    dest.append(source[i-1]);
-                                    }
-                catch(NumberFormatException nfe)
-                {
-                    dest.append(source[i]);
-                    i++;
-                }*/
+                    String el = String.valueOf(source[i]);
 
-            //}
-            System.out.print(dest.toString());
+                    int number = 0;
+                    try {
+                        number = Integer.parseInt(el);
+                    } catch (NumberFormatException e) {
+                        dest.append(source[i]);
+                        continue;
+                    }
+
+                
+
+
+                    for (int j = 1; j < number; j++) {
+                        dest.append(source[i - 1]);
+                    }
+                }
+
+                System.out.print(dest.toString());
+            }
         }
     }
 
-}
